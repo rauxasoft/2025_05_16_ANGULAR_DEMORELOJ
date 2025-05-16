33d443b8806e00bf11a87186971334ebdccf28b4
jordi from virtual machine <jordi@gmail.com>
2025-05-16 19:10:49 +0200

    se quitan imports innecesarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, signal } from '@angular/core';
-import { CronoEngineComponent } from './cronometro/crono-engine/crono-engine.component';
-import { ChronoEvent } from './cronometro/chrono-event.model';
+import { Component } from '@angular/core';
 import { CronoConsoleComponent } from "./cronometro/crono-console/crono-console.component";
 
 @Component({
